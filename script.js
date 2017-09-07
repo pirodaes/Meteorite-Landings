f40d143fc1a7c1de8bf74fbde17e36379afecf58
pyrodaes <pirodaes@gmail.com>
2017-09-07 20:04:08 +0200

    first bubble chart and radio buttons

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ d3.csv("data/csv-generated.csv", function(error, data) {
 // BAR CHART
 
   var w = 1000;
-  var h = 1600;
-  var barSvg = d3.select("body").append("svg")
+  var h = 920;
+  var barSvg = d3.select("body").select("#barChart").append("svg")
     .attr("width", w)
     .attr("height", h)
-    .attr("id", "barChart");
+    .attr("id", "barSvg");
 
 // nesting original data: grouping identical year values
   var dataByYear = d3.nest()
@@ -44,8 +44,8 @@ d3.csv("data/csv-generated.csv", function(error, data) {
     });
   console.log(dataByYear);
 
-var barHeight = 9;
-var barPadding = 4;
+var barHeight = 8;
+var barPadding = 5;
 
 // drawing bars with transition and skew
   var bars = barSvg.selectAll("rect")
@@ -57,7 +57,7 @@ var barPadding = 4;
       })
       .attr("x", w/2)
       .attr("y", function(d, i) {
-            return i * (barHeight + barPadding);
+            return 10 + i * (barHeight + barPadding);
       })
       .attr("fill", "white")
       .attr("height", barHeight)
@@ -81,12 +81,13 @@ var barPadding = 4;
             return w/2 + 15;
       })
       .attr("y", function(d, i) {
-            return 10 + i * 13;
+            return 20 + i * 13;
       })
       .attr("font-family", "sans-serif")
       .attr("font-size", "11px")
       .attr("fill", "white")
-      .attr("text-anchor", "start");
+      .attr("text-anchor", "start")
+      .style("cursor", "pointer");
 
 // drawing container for hoverList
 var listXPos = w/2 + 100;
@@ -100,7 +101,7 @@ var listXPos = w/2 + 100;
             return "list-y" + d.key;
       })
       .attr("y", function(d, i) {
-            return i * 13 - 10;
+            return i * 13;
           })
       .attr("opacity", "0");
 
@@ -113,10 +114,21 @@ var listXPos = w/2 + 100;
     .attr("y", 15)
     .attr("font-family", "sans-serif")
     .attr("font-size", "15px")
-    .attr("fill", "white")
-    .text(function(d) {
-          return "Six heaviest meteorites of " + d.values.length + " total";
-    });
+    .append("tspan")
+      .attr("fill", "white")
+      .text(function(d) {
+            return "Six heaviest meteorites of ";
+      })
+    .append("tspan")
+      .attr("fill", "#ff2b2a")
+      .text(function(d) {
+            return d.values.length;
+      })
+    .append("tspan")
+      .attr("fill", "white")
+      .text(function(d) {
+            return " total";
+      });
 
 // drawing lines inside hoverList
   hoverList.append("line")
@@ -133,7 +145,7 @@ var listXPos = w/2 + 100;
           return 20;
     })
     .attr("stroke-width", 0.5)
-    .attr("stroke", "white");
+    .attr("stroke", "#999999");
 
 // drawing list entries
   hoverList.selectAll("text.listEntry")
@@ -171,37 +183,87 @@ var listXPos = w/2 + 100;
 // interactivity
   labels.on("mouseover", function() {
           barSvg.selectAll("." + this.getAttribute('class'))
-            .attr("fill", "orange");
-          function getList (d){
-                  return "#list" + d.key;
-          };
+            .transition()
+            .duration(50)
+            .attr("fill", "#ff2b2a");
           barSvg.select("#list-" + this.getAttribute("class"))
             .transition()
-            .duration(200)
+            .duration(100)
             .attr("opacity","1");
-      });
+  });
   labels.on("mouseout", function() {
           barSvg.selectAll("." + this.getAttribute('class'))
             .transition()
-            .duration(100)
+            .duration(50)
             .attr("fill", "white");
           barSvg.select("#list-" + this.getAttribute("class"))
             .transition()
-            .duration(200)
+            .duration(100)
             .attr("opacity","0");
-      });
-
-  bars.transition()
-  .duration(800)
-  .delay(function(d, i) {
-        return i * 50;
-  })
-  .attr("width", function(d) {
-        return d.values.length/8;
-  })
-  .attr("x", function(d, i) {
-        return w/2 - d.values.length/8;
   });
+
+// load bar chart
+  barSvg.on("mouseover", function(){
+        barSvg.selectAll("rect")
+        .transition()
+          .duration(500)
+          .delay(function(d, i) {
+                return i * 10;
+          })
+          .attr("width", function(d) {
+                return d.values.length/8;
+          })
+          .attr("x", function(d, i) {
+                return w/2 - d.values.length/8;
+          });
+  });
+
+
+// BUBBLE CHART
+
+  var bubbleSvg = d3.select("body").select("#bubbleChart").append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .attr("id", "bubbleSvg");
+
+// nest data by rectype
+  var dataByType = d3.nest()
+    .key(function(d) {
+          return d.rectype;
+    })
+    .entries(dataset) // the original array
+  console.log(dataByType);
+
+// simulate physics
+  var simulation = d3.forceSimulation(dataByType)
+    .force("charge", d3.forceManyBody().strength([-20]))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
+    .on("tick", ticked); // updates the position of each circle (from function to DOM)
+
+// call to check the position of each circle
+  function ticked(e) {
+        node.attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+  };
+
+// create scale to link radius and mass
+  var scaleRadius = d3.scaleLinear()
+            .domain([d3.min(dataByType, function(d) { return d.values.mass; }),
+                    d3.max(dataByType, function(d) { return d.values.mass; })])
+            .range([2,18]);
+
+// create range of colors based on tectype
+  var colorCircles = d3.scaleOrdinal(d3.schemeCategory10);
+
+// draw circles
+  var node = bubbleSvg.selectAll("circle")
+     .data(dataset)
+     .enter()
+     .append("circle")
+     .attr('r', function(d) { return scaleRadius(d.values.mass)})
+     .attr("fill", function(d) { return colorCircles(d.key)})
+     .attr('transform', 'translate(' + [w/2, h/2] + ')');
 
 
     } // closes else
